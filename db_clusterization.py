9ee78c609f9a43a550ac766ffa9e6df5e77959c0
peppermint-ice <d.usenko@hotmail.com>
2023-09-18 17:56:30 +0300

    all files

--- db_clusterization.py
+++ db_clusterization.py
@@ -10,9 +10,9 @@ import random
 
 def open_ply_file(file_path):
     """ This function opens a ply file and returns it as a numpy array """
-    pcd = o3d.io.read_point_cloud(file_path)
-    point_cloud_data_array = np.asarray(pcd.points)
-    return point_cloud_data_array
+    point_cloud_data_file = o3d.io.read_point_cloud(file_path)
+    point_cloud_data_array = np.asarray(point_cloud_data_file.points)
+    return point_cloud_data_array, point_cloud_data_file
 
 
 def create_clusters(point_cloud_data, eps=0.15, min_samples=80):
@@ -128,22 +128,49 @@ def export_clustered_data(point_cloud_data, cluster_labels, file_path):
     vertex_data['green'][noise_mask] = noise_color[1]
     vertex_data['blue'][noise_mask] = noise_color[2]
 
-    # Create a PLY file
-    ply = plyfile.PlyData([
-        plyfile.PlyElement.describe(vertex_data, 'vertex', comments=['red', 'green', 'blue'])
-    ])
+    # # Create a PLY file
+    # ply = plyfile.PlyData([
+    #     plyfile.PlyElement.describe(vertex_data, 'vertex', comments=['red', 'green', 'blue'])
+    # ])
+    #
+    # # Save the PLY file
+    # ply.write(file_path)
 
-    # Save the PLY file
-    ply.write(file_path)
+    with open(file_path, 'w') as f:
+        # Create a PLY file object in write mode
+        ply_data = plyfile.PlyData([
+            plyfile.PlyElement.describe(vertex_data, 'vertex', comments=['red', 'green', 'blue'])], text=True)
+
+        # Write the PLY data to the file
+        ply_data.write(f)
 
 
-def run_dbscan(input_path, output_path, plot=False, eps=0.15, min_samples=80):
+def export_clustered_data_no_blanks(point_cloud_file, cluster_labels, file_path):
+    # Extract the vertices (points) and colors
+    vertices = np.asarray(point_cloud_file.points)
+    colors = np.asarray(point_cloud_file.colors)
+    # Filter points that are assigned to clusters (not -1)
+    clustered_indices = np.where(cluster_labels != -1)[0]
+    clustered_points = vertices[clustered_indices]
+    clustered_colors = colors[clustered_indices]
+    # Create a new point cloud with clustered points and colors
+    clustered_point_cloud = o3d.geometry.PointCloud()
+    clustered_point_cloud.points = o3d.utility.Vector3dVector(clustered_points)
+    clustered_point_cloud.colors = o3d.utility.Vector3dVector(clustered_colors)
+    # Save the clustered point cloud to a new PLY file
+    o3d.io.write_point_cloud(file_path, clustered_point_cloud, write_ascii=True)
+
+
+def run_dbscan(input_path, output_path, plot=False, original_colors=False, eps=0.15, min_samples=80):
     """This function runs the whole workflow. Plotting is turned off by default """
-    point_cloud_array = open_ply_file(input_path)
+    point_cloud_array, point_cloud_file = open_ply_file(input_path)
     cluster_names = create_clusters(point_cloud_array, eps, min_samples)
     unique_cluster_labels = count_unique_cluster_labels(cluster_names)
     stats = count_all_cluster_sizes(cluster_names, unique_cluster_labels)
-    export_clustered_data(point_cloud_array, cluster_names, output_path)
+    if original_colors:
+        export_clustered_data_no_blanks(point_cloud_file, cluster_names, output_path)
+    else:
+        export_clustered_data(point_cloud_array, cluster_names, output_path)
     if plot:
         plot_clusters_3d(point_cloud_array, cluster_names)
     return stats
