6be2ed440bc84d8cb07a77a9269534034ebe79f2
peppermint-ice <d.usenko@hotmail.com>
2024-03-21 18:01:52 +0200

    all files

--- pipelines/create_alpha.py
+++ pipelines/create_alpha.py
@@ -1,9 +1,6 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from dbscanning_310 import leaf_area as la
+import leaf_area as la
 from config import paths
-
+import os
 import re
 import pandas as pd
 
