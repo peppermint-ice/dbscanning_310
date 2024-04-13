2bfd4b9edc8ecde828ae45e9db0e19a45d7d3576
peppermint-ice <d.usenko@hotmail.com>
2024-04-14 00:43:37 +0300

    slurm_stats

--- stats/svm.py
+++ stats/svm.py
@@ -32,6 +32,8 @@ def load_train_test_sets(df, target_column='Measured_leaf_area', by_year=False):
                               inplace=False, axis=1)
         y_test = df_test[target_column]
     else:
+        df.drop(columns=['File_name', 'parameter_type', 'parameter_value', 'Year'],
+                inplace=True, axis=1)
         X_train, X_test, y_train, y_test = train_test_split(df.drop(target_column, axis=1), df[target_column],
                                                             test_size=0.3, random_state=42)
     # print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
