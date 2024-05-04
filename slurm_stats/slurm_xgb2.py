7f5da76694d87bdadda1c10b3733e879e67c9530
peppermint-ice <d.usenko@hotmail.com>
2024-05-05 01:04:29 +0300

    slurm_stats

--- slurm_stats/slurm_xgb2.py
+++ slurm_stats/slurm_xgb2.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import os
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 from scipy.stats import randint, uniform
 from sklearn.model_selection import RandomizedSearchCV
-from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 from config import paths
@@ -21,17 +21,17 @@ def preprocess_data(df, target_column='Measured_leaf_area', by_year=False):
     df_train = df[df['Year'] == 2023]
     df_test = df[df['Year'] == 2024]
     if by_year:
-        X_train = df_train.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year'],
+        X_train = df_train.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year', 'Surface_area', 'Elongation', 'Flatness', 'Sphericity', 'Compactness'],
                                 inplace=False, axis=1)
         y_train = df_train[target_column]
-        X_test = df_test.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year'],
+        X_test = df_test.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year', 'Surface_area', 'Elongation', 'Flatness', 'Sphericity', 'Compactness'],
                               inplace=False, axis=1)
         y_test = df_test[target_column]
     else:
-        df.drop(columns=['File_name', 'parameter_type', 'parameter_value', 'Year'],
+        df.drop(columns=['File_name', 'parameter_type', 'parameter_value', 'Year', 'Surface_area', 'Elongation', 'Flatness', 'Sphericity', 'Compactness'],
                 inplace=True, axis=1)
         # Define the number of quantiles
-        num_quantiles = 10
+        num_quantiles = int(10)
 
         # Assuming df is your DataFrame
         # First, divide the DataFrame into quantiles based on the target column
@@ -73,22 +73,46 @@ def preprocess_data(df, target_column='Measured_leaf_area', by_year=False):
     # Center and scale the data
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
+    scaler = StandardScaler()
     X_test_scaled = scaler.fit_transform(X_test)
 
-    return X_train_scaled, X_test_scaled, y_train, y_test, [parameter_type, parameter_value]
+    return X_train, X_test, y_train, y_test, [parameter_type, parameter_value]
+
+
+def bootstrap_sample(X, y, num_samples=100):
+    sample_indices = np.random.choice(range(len(X)), size=(num_samples, len(X)), replace=True)
+    X_samples = [X[idx] for idx in sample_indices]
+    y_samples = [y[idx] for idx in sample_indices]
+    return X_samples, y_samples
+
+
+def plot_predictions(y_true, y_pred, r2, parameter_value, parameter_type):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(y_true, y_pred, color='blue')
+    plt.plot([y_true.min(), y_true.max()], [y_true.min(), y_true.max()], 'k--', lw=4)
+    plt.xlabel('Measured Leaf Area')
+    plt.ylabel('Predicted Leaf Area')
+    plt.title('Leaf Area Prediction: XGBoost\nR2 Score: {:.2f}'.format(r2))
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(f'{parameter_value}_{parameter_type}_prediction_plot.png')
+    plt.show()
 
 
 if __name__ == '__main__':
-    # Get paths
+    # Get the file path from the command-line argument
+    file_path = sys.argv[1]
+
     folder_paths = paths.get_paths()
-    csv_export_path = os.path.join(folder_paths["hyperparameters"], 'xgb_marching_cubess0_9.csv')
-    csv_import_path = os.path.join(folder_paths["reconstructions_by_parameters"], 'marching_cubess0_9.csv')
-    df = pd.read_csv(csv_import_path)
+    csv_folder_path = folder_paths["ml_results"]
+
+    # Read the CSV file specified by the command-line argument
+    df = pd.read_csv(file_path)
 
     print(df['parameter_type'].unique())
 
     # First run of train-test split to set the desired columns
-    X_train, X_test, y_train, y_test, values = preprocess_data(df, by_year=False)
+    X_train, X_test, y_train, y_test, values = preprocess_data(df, by_year=True)
 
     keys = [
         'Parameter_name',
@@ -102,7 +126,7 @@ if __name__ == '__main__':
         'Successful_reconstructions_test',
         'Successful_reconstructions_train']
     current_results = dict.fromkeys(keys)
-    results_xbg = pd.DataFrame()
+    results_xgb = pd.DataFrame()
     try:
         print('starting grid search')
         # Define distributions for hyperparameters
@@ -124,46 +148,49 @@ if __name__ == '__main__':
 
         # Refactor model training to use the best hyperparameters
         model = XGBRegressor(**best_params)
-        model.fit(X_train, y_train)
 
-        pred_cal = model.predict(X_train)
-        pred_val = model.predict(X_test)
-        mse_cal = mean_squared_error(y_train, pred_cal)
-        mse_val = mean_squared_error(y_test, pred_val)
-        r2_cal = r2_score(y_train, pred_cal)
-        r2_val = r2_score(y_test, pred_val)
+        # Bootstrap sampling
+        X_train_samples, y_train_samples = bootstrap_sample(X_train, y_train)
 
-        # print(parameter_type, ': ', parameter_value)
-        # print('Correlating parameter: ', column)
-        # print('r2 cal: ', r2_cal)
-        # print('r2 val: ', r2_val)
-        # print('')
+        mse_cal = []
+        mse_val = []
+        r2_cal = []
+        r2_val = []
 
-        # Plot predicted vs real values
-        plt.figure(figsize=(10, 6))
-        plt.scatter(y_test, pred_val, color='blue', label='Predicted vs Real')
-        plt.plot(y_test, y_test, color='red', linewidth=2, label='Ideal Line')
-        plt.title('Predicted vs Real Values')
-        plt.xlabel('Real Values')
-        plt.ylabel('Predicted Values')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
+        for X_train_sample, y_train_sample in zip(X_train_samples, y_train_samples):
+            model.fit(X_train_sample, y_train_sample)
+            pred_cal = model.predict(X_train_sample)
+            pred_val = model.predict(X_test)
+            mse_cal.append(mean_squared_error(y_train_sample, pred_cal))
+            mse_val.append(mean_squared_error(y_test, pred_val))
+            r2_cal.append(r2_score(y_train_sample, pred_cal))
+            r2_val.append(r2_score(y_test, pred_val))
 
-        # parameter_value = df['parameter_value'].unique()[0]
-        # parameter_type = df['parameter_type'].unique()[0]
-        #
-        # current_results['Parameter_value'] = parameter_value
-        # current_results['Parameter_name'] = parameter_type
+        mse_cal_mean = np.mean(mse_cal)
+        mse_val_mean = np.mean(mse_val)
+        r2_cal_mean = np.mean(r2_cal)
+        r2_val_mean = np.mean(r2_val)
+
+        parameter_value = values[1]
+        parameter_type = values[0]
+
+        current_results['Parameter_value'] = parameter_value
+        current_results['Parameter_name'] = parameter_type
         current_results['Regression_model'] = 'XGBoost'
-        current_results['RMSE_score_calibration'] = mse_cal
-        current_results['RMSE_score_validation'] = mse_val
-        current_results['R2_score_calibration'] = r2_cal
-        current_results['R2_score_validation'] = r2_val
+        current_results['RMSE_score_calibration'] = mse_cal_mean
+        current_results['RMSE_score_validation'] = mse_val_mean
+        current_results['R2_score_calibration'] = r2_cal_mean
+        current_results['R2_score_validation'] = r2_val_mean
         current_results['Successful_reconstructions_test'] = len(X_test)
         current_results['Successful_reconstructions_train'] = len(X_train)
-        results_xbg = pd.concat([results_xbg, pd.DataFrame([current_results])], ignore_index=True)
+        results_xgb = pd.concat([results_xgb, pd.DataFrame([current_results])], ignore_index=True)
+        print(results_xgb.shape)
+        output_file = f'{parameter_value}_{parameter_type}_results_xgb.csv'
+        output_file_path = os.path.join(csv_folder_path, output_file)
+        results_xgb.to_csv(output_file_path, index=False)
+
+        # Plot predictions
+        plot_predictions(y_test, model.predict(X_test), r2_val_mean, parameter_value, parameter_type)
+
     except ValueError:
         print('A small dataset. Cannot calculate')
-    print(results_xbg.to_string())
-    results_xbg.to_csv(csv_export_path, index=False)
