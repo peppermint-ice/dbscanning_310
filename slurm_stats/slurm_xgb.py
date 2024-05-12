2f1add8f88cf262f7fbf1c605597980a4ecc1233
peppermint-ice <d.usenko@hotmail.com>
2024-05-12 10:59:42 +0300

    all files

--- slurm_stats/slurm_xgb.py
+++ slurm_stats/slurm_xgb.py
@@ -8,74 +8,37 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from scipy.stats import randint, uniform
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.preprocessing import StandardScaler
 
 from config import paths
 
 
-def preprocess_data(df, target_column='Measured_leaf_area', by_year=False):
-    parameter_type = df['parameter_type'].unique()[0]
-    parameter_value = df['parameter_value'].unique()[0]
+def load_train_test_sets(df, target_column='Measured_leaf_area', by_year=False):
+    '''
+    A function to load train and test data sets based on a target column and year.
+    :param df: dataframe containing reconstruction results for one parameter name and value
+    :param target_column: i guess, leaf area. might be changed to whatever is needed.
+    :param by_year: if true, allows to train models on year 2023 and test on year 2024.
+    :return: 4 dataframes containing train and test data sets.
+    '''
     df.loc[:, 'Year'] = ('20' + df['File_name'].str[:6].str[-2:]).astype(int)
     df_train = df[df['Year'] == 2023]
     df_test = df[df['Year'] == 2024]
     if by_year:
-        X_train = df_train.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year', 'Surface_area', 'Elongation', 'Flatness', 'Sphericity', 'Compactness'],
+        X_train = df_train.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year'],
                                 inplace=False, axis=1)
         y_train = df_train[target_column]
-        X_test = df_test.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year', 'Surface_area', 'Elongation', 'Flatness', 'Sphericity', 'Compactness'],
+        X_test = df_test.drop(columns=[target_column, 'File_name', 'parameter_type', 'parameter_value', 'Year'],
                               inplace=False, axis=1)
         y_test = df_test[target_column]
     else:
-        df.drop(columns=['File_name', 'parameter_type', 'parameter_value', 'Year', 'Surface_area', 'Elongation', 'Flatness', 'Sphericity', 'Compactness'],
-                inplace=True, axis=1)
-        # Define the number of quantiles
-        num_quantiles = int(10)
+        df.drop(columns=['File_name', 'parameter_type', 'parameter_value', 'Year'],
+                      inplace=True, axis=1)
+        X_train, X_test, y_train, y_test = train_test_split(df.drop(target_column, axis=1), df[target_column],
+                                                            test_size=0.3, random_state=42)
+    # print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+    # print(X_train, X_test, y_train, y_test)
 
-        # Assuming df is your DataFrame
-        # First, divide the DataFrame into quantiles based on the target column
-        df['quantile'] = pd.qcut(df[target_column], q=num_quantiles, labels=False)
-
-        # Initialize empty lists to store train and test sets
-        X_train_list = []
-        X_test_list = []
-        y_train_list = []
-        y_test_list = []
-
-        # Iterate over each quantile
-        for quantile in range(num_quantiles):
-            # Filter the DataFrame for the current quantile
-            df_quantile = df[df['quantile'] == quantile]
-
-            # Split the quantile into train and test sets
-            X_train_q, X_test_q, y_train_q, y_test_q = train_test_split(df_quantile.drop(target_column, axis=1),
-                                                                        df_quantile[target_column],
-                                                                        test_size=0.3,
-                                                                        random_state=42)
-
-            # Append the train and test sets to the lists
-            X_train_list.append(X_train_q)
-            X_test_list.append(X_test_q)
-            y_train_list.append(y_train_q)
-            y_test_list.append(y_test_q)
-
-        # Concatenate all the train and test sets
-        X_train = pd.concat(X_train_list)
-        X_test = pd.concat(X_test_list)
-        y_train = pd.concat(y_train_list)
-        y_test = pd.concat(y_test_list)
-
-        # Drop the 'quantile' column from the final datasets
-        X_train.drop('quantile', axis=1, inplace=True)
-        X_test.drop('quantile', axis=1, inplace=True)
-
-    # Center and scale the data
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    scaler = StandardScaler()
-    X_test_scaled = scaler.fit_transform(X_test)
-
-    return X_train, X_test, y_train, y_test, [parameter_type, parameter_value]
+    return X_train, X_test, y_train, y_test
 
 
 if __name__ == '__main__':
@@ -91,7 +54,7 @@ if __name__ == '__main__':
     print(df['parameter_type'].unique())
 
     # First run of train-test split to set the desired columns
-    X_train, X_test, y_train, y_test, values = preprocess_data(df, by_year=True)
+    X_train, X_test, y_train, y_test = load_train_test_sets(df, by_year=True)
 
     keys = [
         'Parameter_name',
@@ -136,10 +99,14 @@ if __name__ == '__main__':
         r2_cal = r2_score(y_train, pred_cal)
         r2_val = r2_score(y_test, pred_val)
 
+        # print(parameter_type, ': ', parameter_value)
+        # print('Correlating parameter: ', column)
+        # print('r2 cal: ', r2_cal)
+        # print('r2 val: ', r2_val)
+        # print('')
 
-        parameter_value = values[1]
-        parameter_type = values[0]
-
+        parameter_value = df['parameter_value'].unique()[0]
+        parameter_type = df['parameter_type'].unique()[0]
 
         current_results['Parameter_value'] = parameter_value
         current_results['Parameter_name'] = parameter_type
